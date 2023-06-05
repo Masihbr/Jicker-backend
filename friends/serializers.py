1fae4780b2f6ecce6f47cbf4d3177de1b98cae9f
masihbr <masihbr@gmail.com>
2023-06-05 16:12:14 +0430

    Fix following after delete

--- friends/serializers.py
+++ friends/serializers.py
@@ -11,10 +11,24 @@ class FriendCreateSerializer(serializers.ModelSerializer):
     def validate_followed(self, value):
         if value.pk == self.context['request'].user.pk:
             raise serializers.ValidationError('You cannot follow yourself')
+        if friend_models.Connection.objects.filter(
+            follower=self.context['request'].user,
+            followed=value,
+            deleted=False,
+        ).exists():
+            raise serializers.ValidationError('You are already following this user')
         return value
     
     def create(self, validated_data):
-        return super().create({**validated_data, 'follower': self.context['request'].user})
+        follower = self.context['request'].user
+        connection = friend_models.Connection.objects.filter(
+            follower=follower,
+            followed=validated_data['followed'],
+            deleted=True,
+        ).last()
+        if connection is not None:
+            return super().update(connection, {'deleted': False})
+        return super().create({**validated_data, 'follower': follower})
 
 
 class FriendGetSerializer(serializers.ModelSerializer):
@@ -25,5 +39,5 @@ class FriendGetSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = friend_models.Connection
-        fields = ('follower', 'followed', 'created', 'updated')
+        fields = ('id', 'follower', 'followed', 'created', 'updated')
         read_only_fields = ('follower',)
